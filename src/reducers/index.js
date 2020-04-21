2f0dea6f266c6d0d6af6cfc580122bcde6c66cb8
jdi011 <jdi011@jdi011s-Mac-mini.local>
2020-04-22 05:13:31 +0530

    fetch post

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,7 @@
 import { combineReducers } from 'redux';
+import postReducer from './postReducer';
 
 export default combineReducers({
-    replaceME : () => 10
+    // replaceME : () => 10
+    posts: postReducer,
 });
